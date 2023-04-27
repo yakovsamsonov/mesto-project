dfac64cfb959963c167abf6440ce8e3978f3cf97
Anastasia Proskuryakova <anproskuryakova@ozon.ru>
2023-04-27 19:57:13 +0600

    optimized api class

--- src/scripts/components/Api.js
+++ src/scripts/components/Api.js
@@ -3,77 +3,74 @@ export default class Api {
     this.options = options;
   }
 
-  checkResponse(response) {
+  _request(endpoint, options) {
+    return fetch(`${this.options.baseUrl}${endpoint}`, options).then(this._checkResponse);
+  }
+
+  _checkResponse(response) {
     if (response.ok) {
       return response.json();
     } else {
       console.log(`HTTP ошибка! статус: ${response.status}`);
     }
   }
-  
 
   loadProfile() {
-    return fetch(`${this.options.baseUrl}/users/me`, {
+    return this._request('/users/me', {
       headers: this.options.headers
-    }).then(this.checkResponse)
+    });
   }
 
   updateProfile(name, about) {
-    return fetch(`${this.options.baseUrl}/users/me`, {
+    return this._request('/users/me', {
       method: 'PATCH',
       headers: this.options.headers,
       body: JSON.stringify({
         name,
         about
       })
-    })
-      .then(this.checkResponse)
+    });
   }
 
   updateAvatar(avatarImageLink) {
-    return fetch(`${this.options.baseUrl}/users/me/avatar`, {
+    return this._request('/users/me/avatar', {
       method: 'PATCH',
       headers: this.options.headers,
       body: JSON.stringify({
         avatar: avatarImageLink,
       }),
-    })
-      .then(this.checkResponse);
+    });
   }
 
   deleteCard(cardId) {
-    return fetch(`${this.options.baseUrl}/cards/${cardId}`, {
+    return this._request(`/cards/${cardId}`, {
       method: 'DELETE',
       headers: this.options.headers,
-    })
-      .then(this.checkResponse)
+    });
   }
 
   loadCards() {
-    return fetch(`${this.options.baseUrl}/cards`, {
+    return this._request('/cards', {
       headers: this.options.headers
-    })
-      .then(this.checkResponse)
+    });
   }
 
   saveCard(name, link) {
-    return fetch(`${this.options.baseUrl}/cards`, {
+    return this._request('/cards', {
       method: 'POST',
       headers: this.options.headers,
       body: JSON.stringify({
         name,
         link
       })
-    })
-    .then(this.checkResponse)
+    });
   }
 
   toggleLike(cardId, isLiked) {
     const method = isLiked ? 'DELETE' : 'PUT';
-    return fetch(`${this.options.baseUrl}/cards/likes/${cardId}`, {
+    return this._request(`/cards/likes/${cardId}`, {
       method,
       headers: this.options.headers
-    })
-    .then(this.checkResponse);
+    });
   }
 }
