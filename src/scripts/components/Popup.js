354876292128e856205565c8356ebd3ba74c9452
Natalia Doroshenko <Natalica.22@yandex.ru>
2023-04-27 21:05:35 +0700

    fix: handleEscClose bind

--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -1,16 +1,17 @@
 export default class Popup {
   constructor(selector) {
     this._popup = document.querySelector(selector);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
-    document.addEventListener("keydown", this._handleEscClose.bind(this));
+    document.addEventListener("keydown", this._handleEscClose);
     this._popup.classList.add("popup_opened");
   }
 
   close() {
     this._popup.classList.remove("popup_opened");
-    document.removeEventListener("keydown", this._handleEscClose.bind(this));
+    document.removeEventListener("keydown", this._handleEscClose);
   }
 
   _handleEscClose(evt) {
